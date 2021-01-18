03dd0e5c9293e70bb4ac587bb68f6f5f1abceee3
ravas <z.kinshef@gmail.com>
2021-01-18 11:37:45 +0300

    add clean_pr

---
+++ landings/clean_pr/src/scripts/index.js
@@ -0,0 +1,18 @@
+// import '@babel/polyfill'
+// import "core-js/stable";
+// import "regenerator-runtime/runtime";
+import 'font-awesome/css/font-awesome.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'lightbox2/dist/css/lightbox.min.css'
+
+import '../style/main.scss'
+import $ from 'jquery'
+window.$ = $
+import 'popper.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+// import slider from './additional/slider'
+
+window.addEventListener('DOMContentLoaded', () => {
+  // slider()
+})
